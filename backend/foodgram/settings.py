056bc5712c595e97127aca1c0185d51bb8619dd2
Dmitriy S <sdn7177@gmail.com>
2023-03-29 19:52:28 +0300

    settings

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', default='123456')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG_STATUS', default=True)
+DEBUG = os.getenv('DEBUG_STATUS', default=False)
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS_LIST', default=['*'])
 
