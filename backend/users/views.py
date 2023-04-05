42cf53f631663fd4d6ad39a2db36199b3e4d99f8
Dmitriy S <sdn7177@gmail.com>
2023-04-05 14:47:58 +0300

    12345

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,4 +1,5 @@
 from http import HTTPStatus
+
 from django.contrib.auth import get_user_model
 from api.paginators import CustomPagination
 from api.serializers import CustomUserSerializer, SubscribeSerializer
